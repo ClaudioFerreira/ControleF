607cbbf6f1f4e733ea2e552081e7fc837b523135
Claudio Ferreira <claudio.zero0@gmail.com>
2024-03-19 12:20:42 -0300

    feat: inclusao inicial de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './pages/auth/auth.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: AuthComponent },
+  { path: '', pathMatch: 'full', component: HomeComponent },
+];
---
+++ src/app/shared/enums/rotas.enum.ts
@@ -0,0 +1,4 @@
+export enum RotasEnum {
+  'LOGIN' = '/login',
+  'HOME' = '',
+}
