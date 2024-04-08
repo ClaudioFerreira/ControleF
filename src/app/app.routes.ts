bfb445f200573e0728e45b1c46c9a8f23c5bdd2f
Claudio Ferreira <claudio.zero0@gmail.com>
2024-04-08 16:51:13 -0300

    feat(rotas): add rota cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
+import { BillsRegistrationComponent } from './pages/bills-registration/bills-registration.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   { path: 'login', component: AuthComponent },
   { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'cadastro-contas', component: BillsRegistrationComponent },
   { path: 'configuracoes', component: HomeComponent },
   { path: '**', redirectTo: 'login', pathMatch: 'full' },
 ];
--- src/app/shared/enums/rotas.enum.ts
+++ src/app/shared/enums/rotas.enum.ts
@@ -1,5 +1,6 @@
 export enum RotasEnum {
   'LOGIN' = '/login',
   'HOME' = '',
+  'CADASTRO_CONTAS' = '/cadastro-contas',
   'CONFIGURACOES' = '/configuracoes',
 }
