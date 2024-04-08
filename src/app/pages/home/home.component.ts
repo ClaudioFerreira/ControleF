93c6d5db99a1eced2c8d1756f041b3c9d71a6878
Claudio Ferreira <claudio.zero0@gmail.com>
2024-04-08 12:00:11 -0300

    feat(layout): conceito do layout inicial da home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../shared/components/card/card.component';
 import { SidebarComponent } from '../../shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  acompanhamentos = [
+    {
+      nome: 'Reserva emergencia',
+      valor: '123,12',
+    },
+    {
+      nome: 'Investimentos',
+      valor: '123,12',
+    },
+    {
+      nome: 'Card 3',
+      valor: '123,12',
+    },
+    {
+      nome: 'Card 4',
+      valor: '123,12',
+    },
+  ];
+}
